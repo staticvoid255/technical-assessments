88d76505582baab2e9cf5bf496158b0ced55d528
Static Void <staticvoidnetwork@outlook.com>
2020-09-08 12:21:22 +0200

    import boilerplatw aci deployment

---
+++ Greenflux/src/azure-app-aci-generic.tf
@@ -0,0 +1,31 @@
+resource "azurerm_container_group" "aci_cg_test" {
+  name                = "aci-cg-test"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  ip_address_type     = "public"
+  dns_name_label      = "aci-label"
+  os_type             = "Linux"
+
+  container {
+    name   = "hello-world"
+    image  = "microsoft/aci-helloworld:latest"
+    cpu    = "0.5"
+    memory = "1.5"
+
+    ports {
+      port     = 443
+      protocol = "TCP"
+    }
+  }
+
+  container {
+    name   = "sidecar"
+    image  = "microsoft/aci-tutorial-sidecar"
+    cpu    = "0.5"
+    memory = "1.5"
+  }
+
+  tags = {
+    environment = "testing"
+  }
+}
