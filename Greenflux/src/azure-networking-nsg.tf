fbfbd5a42be4d6de452fa8b8c35f87fe6c637dac
Static Void <staticvoidnetwork@outlook.com>
2020-09-08 12:15:48 +0200

    add vnet for test environment and rename network security gateway

--- Greenflux/src/azure-networking-nsg.tf
+++ Greenflux/src/azure-networking-nsg.tf
@@ -1,4 +1,4 @@
-resource "azurerm_network_security_group" "example" {
+resource "azurerm_network_security_group" "nsg_default" {
   name                = var.network_security_group_name
   location            = var.location
   resource_group_name = var.resource_group_name
---
+++ Greenflux/src/azure-networking-vnet.tf
@@ -0,0 +1,18 @@
+resource "azurerm_virtual_network" "vnet_test" {
+  name                = "solar-impulse-test"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  address_space       = ["10.0.0.0/16"]
+  dns_servers         = ["10.0.0.4", "10.0.0.5"]
+
+  subnet {
+    name           = "subnet-default"
+    address_prefix = "10.0.3.0/24"
+    security_group = azurerm_network_security_group.nsg_default.id
+  }
+
+  tags = {
+    environment = "Test"
+  }
+}
+
