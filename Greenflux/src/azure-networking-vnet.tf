a4f0ecc48920ecca11f2bc704b676bc427ece4ee
Static Void <staticvoidnetwork@outlook.com>
2020-09-08 13:23:00 +0200

    last commit cont'd

--- Greenflux/src/azure-networking-vnet.tf
+++ Greenflux/src/azure-networking-vnet.tf
@@ -22,22 +22,6 @@ resource "azurerm_virtual_network" "test" {
   }
 }
 
-resource "azurerm_virtual_network_gateway" "test" {
-  name                = "test-gateway"
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
-
-  type     = "Vpn"
-  vpn_type = "RouteBased"
-  sku      = "Basic"
-
-  ip_configuration {
-    public_ip_address_id          = azurerm_public_ip.us.id
-    private_ip_address_allocation = "Dynamic"
-    subnet_id                     = azurerm_subnet.us_gateway.id
-  }
-}
-
 resource "azurerm_virtual_network" "vnet_acc" {
   name                = "solar-impulse-acc"
   location            = var.location
