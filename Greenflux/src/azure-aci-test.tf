03cbaf5c3ee8641c8616c239b2c2e36613f0d836
Static Void <staticvoidnetwork@outlook.com>
2020-09-08 12:39:41 +0200

    configure dnsmasq aci deployment to test and rename for specificity

---
+++ Greenflux/src/azure-aci-test.tf
@@ -0,0 +1,31 @@
+resource "azurerm_container_group" "aci_cg_test" {
+  name                = "aci-cg-test"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  ip_address_type     = "public"
+  dns_name_label      = "aci-test"
+  os_type             = "Linux"
+
+  container {
+    name   = "hello-world"
+    image  = "strm/dnsmasq:latest"
+    cpu    = "1"
+    memory = "2"
+
+    environment_variables = {
+        "server": "8.8.8.8",
+        "server": "8.8.4.4"
+    } 
+
+    secure_environment_variables = [] //Use for sensitive data
+
+    ports {
+      port     = 53 //Port 53 also specified in Dockerfile
+      protocol = "UDP"
+    }
+  }
+
+  tags = {
+    environment = "Test"
+  }
+}
