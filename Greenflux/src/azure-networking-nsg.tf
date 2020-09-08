1b0e977675d66ff7e47348b02bd8da212f4060ae
Static Void <staticvoidnetwork@outlook.com>
2020-09-08 12:24:10 +0200

    remove references to user-defined variables for child objects of the default resource group - this will prevent terraform from losing track of objects even if renamed manually in the portal

--- Greenflux/src/azure-networking-nsg.tf
+++ Greenflux/src/azure-networking-nsg.tf
@@ -1,5 +1,5 @@
 resource "azurerm_network_security_group" "nsg_default" {
   name                = var.network_security_group_name
-  location            = var.location
-  resource_group_name = var.resource_group_name
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
 }
--- Greenflux/src/azure-networking-vnet.tf
+++ Greenflux/src/azure-networking-vnet.tf
@@ -1,7 +1,7 @@
 resource "azurerm_virtual_network" "vnet_test" {
   name                = "solar-impulse-test"
-  location            = var.location
-  resource_group_name = var.resource_group_name
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
   address_space       = ["10.0.0.0/16"]
   dns_servers         = ["10.0.0.4", "10.0.0.5"]
 
