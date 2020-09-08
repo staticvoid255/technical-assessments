1e9288de91518d419affacd55fe317f884846762
Static Void <staticvoidnetwork@outlook.com>
2020-09-08 13:12:03 +0200

    split subnet ip ranges

--- Greenflux/src/azure-networking-vnet.tf
+++ Greenflux/src/azure-networking-vnet.tf
@@ -32,18 +32,18 @@ resource "azurerm_virtual_network" "vnet_acc" {
   name                = "solar-impulse-acc"
   location            = var.location
   resource_group_name = var.resource_group_name
-  address_space       = ["10.0.0.0/16"]
-  dns_servers         = ["10.0.0.4", "10.0.0.5"]
+  address_space       = ["10.1.0.0/16"]
+  dns_servers         = ["10.1.0.4", "10.1.0.5"]
 
   subnet {
     name           = "subnet-gateway-test"
-    address_prefix = "10.0.1.0/24"
+    address_prefix = "10.1.1.0/24"
     security_group = azurerm_network_security_group.nsg_default.id
   }
 
   subnet {
     name           = "subnet-default"
-    address_prefix = "10.0.2.0/24"
+    address_prefix = "10.1.2.0/24"
     security_group = azurerm_network_security_group.nsg_default.id
   }
 
