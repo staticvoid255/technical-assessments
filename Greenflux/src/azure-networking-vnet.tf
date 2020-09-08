51159c9dbed1385e6300c2d9e180e9eb1ac59e22
Static Void <staticvoidnetwork@outlook.com>
2020-09-08 12:17:31 +0200

    add acceptance vnet

--- Greenflux/src/azure-networking-vnet.tf
+++ Greenflux/src/azure-networking-vnet.tf
@@ -16,3 +16,21 @@ resource "azurerm_virtual_network" "vnet_test" {
   }
 }
 
+resource "azurerm_virtual_network" "vnet_acc" {
+  name                = "solar-impulse-acc"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  address_space       = ["10.0.0.0/16"]
+  dns_servers         = ["10.0.0.4", "10.0.0.5"]
+
+  subnet {
+    name           = "subnet-default"
+    address_prefix = "10.0.3.0/24"
+    security_group = azurerm_network_security_group.nsg_default.id
+  }
+
+  tags = {
+    environment = "Acceptance"
+  }
+}
+
