7479734d64ebba4ce87f32748279bac5b67f8711
Static Void <staticvoidnetwork@outlook.com>
2020-09-08 13:22:31 +0200

    remove gateway subnets for vnet-to-vnet connection - will use peerings instead

--- Greenflux/src/azure-networking-vnet.tf
+++ Greenflux/src/azure-networking-vnet.tf
@@ -1,4 +1,4 @@
-resource "azurerm_virtual_network" "vnet_test" {
+resource "azurerm_virtual_network" "test" {
   name                = "solar-impulse-test"
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
@@ -6,7 +6,7 @@ resource "azurerm_virtual_network" "vnet_test" {
   dns_servers         = ["10.0.0.4", "10.0.0.5"]
 
   subnet {
-    name           = "subnet-gateway-acc"
+    name           = "subnet-default"
     address_prefix = "10.0.1.0/24"
     security_group = azurerm_network_security_group.nsg_default.id
   }
@@ -17,14 +17,24 @@ resource "azurerm_virtual_network" "vnet_test" {
     security_group = azurerm_network_security_group.nsg_default.id
   }
 
-  subnet {
-    name           = "subnet-default"
-    address_prefix = "10.0.3.0/24"
-    security_group = azurerm_network_security_group.nsg_default.id
-  }
-
   tags = {
     environment = "Test"
+  }
+}
+
+resource "azurerm_virtual_network_gateway" "test" {
+  name                = "test-gateway"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  type     = "Vpn"
+  vpn_type = "RouteBased"
+  sku      = "Basic"
+
+  ip_configuration {
+    public_ip_address_id          = azurerm_public_ip.us.id
+    private_ip_address_allocation = "Dynamic"
+    subnet_id                     = azurerm_subnet.us_gateway.id
   }
 }
 
@@ -36,14 +46,8 @@ resource "azurerm_virtual_network" "vnet_acc" {
   dns_servers         = ["10.1.0.4", "10.1.0.5"]
 
   subnet {
-    name           = "subnet-gateway-test"
-    address_prefix = "10.1.1.0/24"
-    security_group = azurerm_network_security_group.nsg_default.id
-  }
-
-  subnet {
     name           = "subnet-default"
-    address_prefix = "10.1.2.0/24"
+    address_prefix = "10.1.1.0/24"
     security_group = azurerm_network_security_group.nsg_default.id
   }
 
@@ -51,4 +55,6 @@ resource "azurerm_virtual_network" "vnet_acc" {
     environment = "Acceptance"
   }
 }
+
+
 
